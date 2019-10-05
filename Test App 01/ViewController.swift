18435b5ce259e505106d842fc974fa43de69013d
emrizkiem <mr.hobskoy@gmail.com>
2019-10-05 23:12:06 +0700

    Initial Commit

---
+++ Test App 01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test App 01
+//
+//  Created by M. Rizki Maulana on 05/10/19.
+//  Copyright © 2019 M. Rizki Maulana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
